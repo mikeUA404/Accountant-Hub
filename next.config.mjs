9c182b1d2ee373308b96046b082e37ac65be231c
mikeUA404 <mikel1.maher@gmail.com>
2026-05-23 10:46:32 +0300

    d

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Exclude Prisma from edge runtime bundles
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
-  },
-};
-
-export default nextConfig;
